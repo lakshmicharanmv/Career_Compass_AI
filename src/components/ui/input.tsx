0f96110a1c90c7d131b71d47468b95a199a25aca
Lakshmi charan M V <mvlakshmicharan07@gmail.com>
2025-07-28 13:11:37 +0000

    In your input.tsx, ensure the input never switches between uncontrolled

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,6 +13,8 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         )}
         ref={ref}
         {...props}
+        value={props.value ?? ""}
+        onChange={props.onChange}
       />
     )
   }
